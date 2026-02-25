25b3fedcd96ab60e5c8103dfae2b02dbf5abbe56
marko <krznarm26@gmail.com>
2026-02-26 00:51:44 +0100

    feat: Add linting, TypeScript, and shadcn/ui setup

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,9 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import reactSWC from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [reactSWC()],
   test: {
     globals: true,
     environment: 'jsdom',
