ae790b7c6610c60a943cf60f0dcbcbec91c7117b
marko <krznarm26@gmail.com>
2026-03-26 23:44:37 +0100

    feat: Refactor LandingSquadsCard to use Card component for improved layout and styling

--- frontend/src/components/LandingSquadsCard.tsx
+++ frontend/src/components/LandingSquadsCard.tsx
@@ -1,4 +1,11 @@
-import { Box, Chip, Stack, Typography } from "@mui/material";
+import {
+	Avatar,
+	Box,
+	Card,
+	CardContent,
+	Divider,
+	Typography,
+} from "@mui/material";
 
 function LandingSquadsCard() {
 	const bijeli = [
@@ -20,20 +27,77 @@ function LandingSquadsCard() {
 		"Miro",
 	];
 	return (
-		<Stack display="flex" flexDirection="column" gap={2} width="100%">
-			<Typography variant="body1">Bijeli</Typography>
-			<Box display="flex" flexDirection="row" gap={1} alignItems="center">
-				{bijeli.map((player) => (
-					<Chip label={player} />
-				))}
-			</Box>
-			<Typography variant="body1">Crni </Typography>
-			<Box display="flex" flexDirection="row" gap={1} alignItems="center">
-				{crni.map((player) => (
-					<Chip label={player} />
-				))}
-			</Box>
-		</Stack>
+		<Card sx={{ flex: 1 }}>
+			<CardContent sx={{ flexDirection: "row" }}>
+				<Box
+					display="flex"
+					flexDirection="column"
+					alignItems="center"
+					gap={2}
+					flex={1}
+				>
+					<Typography variant="subtitle2">Crni </Typography>
+					<Box
+						display="flex"
+						flexDirection="row"
+						alignItems="center"
+						justifyContent="center"
+						flexWrap="wrap"
+						gap={2}
+					>
+						{crni.map((player) => (
+							<Box
+								display="flex"
+								flexDirection="column"
+								alignItems="center"
+								paddingBlock={1}
+								paddingInline={3}
+								gap={1}
+							>
+								<Avatar>{player.charAt(0)}</Avatar>
+								<Typography variant="body2">
+									{player}
+								</Typography>
+							</Box>
+						))}
+					</Box>
+				</Box>
+				<Divider orientation="vertical" flexItem />
+				<Box
+					display="flex"
+					flexDirection="column"
+					gap={2}
+					alignItems="center"
+					flex={1}
+				>
+					<Typography variant="subtitle2">Bijeli</Typography>
+					<Box
+						display="flex"
+						flexDirection="row"
+						alignItems="center"
+						justifyContent="center"
+						flexWrap="wrap"
+						gap={2}
+					>
+						{bijeli.map((player) => (
+							<Box
+								display="flex"
+								flexDirection="column"
+								alignItems="center"
+								paddingBlock={1}
+								paddingInline={3}
+								gap={1}
+							>
+								<Avatar>{player.charAt(0)}</Avatar>
+								<Typography variant="body2">
+									{player}
+								</Typography>
+							</Box>
+						))}
+					</Box>
+				</Box>
+			</CardContent>
+		</Card>
 	);
 }
 
