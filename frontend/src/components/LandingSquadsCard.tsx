fc905fd9c39406359aef9b9c9aa22a19a499ed6a
marko <krznarm26@gmail.com>
2026-03-26 23:48:25 +0100

    feat: Add badge for specific players in LandingSquadsCard for better visibility

--- frontend/src/components/LandingSquadsCard.tsx
+++ frontend/src/components/LandingSquadsCard.tsx
@@ -1,5 +1,6 @@
 import {
 	Avatar,
+	Badge,
 	Box,
 	Card,
 	CardContent,
@@ -45,21 +46,43 @@ function LandingSquadsCard() {
 						flexWrap="wrap"
 						gap={2}
 					>
-						{crni.map((player) => (
-							<Box
-								display="flex"
-								flexDirection="column"
-								alignItems="center"
-								paddingBlock={1}
-								paddingInline={3}
-								gap={1}
-							>
-								<Avatar>{player.charAt(0)}</Avatar>
-								<Typography variant="body2">
-									{player}
-								</Typography>
-							</Box>
-						))}
+						{crni.map((player) => {
+							if (player === "Vukovarac") {
+								return (
+									<Box
+										display="flex"
+										flexDirection="column"
+										alignItems="center"
+										paddingBlock={1}
+										paddingInline={3}
+										gap={1}
+									>
+										<Badge badgeContent="C" color="primary">
+											<Avatar>{player.charAt(0)}</Avatar>
+										</Badge>
+										<Typography variant="body2">
+											{player}
+										</Typography>
+									</Box>
+								);
+							}
+
+							return (
+								<Box
+									display="flex"
+									flexDirection="column"
+									alignItems="center"
+									paddingBlock={1}
+									paddingInline={3}
+									gap={1}
+								>
+									<Avatar>{player.charAt(0)}</Avatar>
+									<Typography variant="body2">
+										{player}
+									</Typography>
+								</Box>
+							);
+						})}
 					</Box>
 				</Box>
 				<Divider orientation="vertical" flexItem />
@@ -79,21 +102,43 @@ function LandingSquadsCard() {
 						flexWrap="wrap"
 						gap={2}
 					>
-						{bijeli.map((player) => (
-							<Box
-								display="flex"
-								flexDirection="column"
-								alignItems="center"
-								paddingBlock={1}
-								paddingInline={3}
-								gap={1}
-							>
-								<Avatar>{player.charAt(0)}</Avatar>
-								<Typography variant="body2">
-									{player}
-								</Typography>
-							</Box>
-						))}
+						{bijeli.map((player) => {
+							if (player === "Ante") {
+								return (
+									<Box
+										display="flex"
+										flexDirection="column"
+										alignItems="center"
+										paddingBlock={1}
+										paddingInline={3}
+										gap={1}
+									>
+										<Badge badgeContent="C" color="primary">
+											<Avatar>{player.charAt(0)}</Avatar>
+										</Badge>
+										<Typography variant="body2">
+											{player}
+										</Typography>
+									</Box>
+								);
+							}
+
+							return (
+								<Box
+									display="flex"
+									flexDirection="column"
+									alignItems="center"
+									paddingBlock={1}
+									paddingInline={3}
+									gap={1}
+								>
+									<Avatar>{player.charAt(0)}</Avatar>
+									<Typography variant="body2">
+										{player}
+									</Typography>
+								</Box>
+							);
+						})}
 					</Box>
 				</Box>
 			</CardContent>
