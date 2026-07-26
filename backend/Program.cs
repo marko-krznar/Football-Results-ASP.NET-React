9ee9c1c56ab30a4ac9b950e02a9794cb7a33faa6
marko <krznarm26@gmail.com>
2026-07-26 23:40:24 +0200

    feat: Register MatchPlayers, Matches, and Sets services in the dependency injection container

--- backend/Program.cs
+++ backend/Program.cs
@@ -16,6 +16,9 @@ builder.Services.AddScoped<IPlayersService, PlayersService>();
 builder.Services.AddScoped<ISeasonsService, SeasonsService>();
 builder.Services.AddScoped<ITeamsService, TeamsService>();
 builder.Services.AddScoped<ITeamMembersService, TeamMembersService>();
+builder.Services.AddScoped<IMatchPlayersService, MatchPlayersService>();
+builder.Services.AddScoped<IMatchesService, MatchesService>();
+builder.Services.AddScoped<ISetsService, SetsService>();
 
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
